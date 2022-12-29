b65728275345b0523f867a23a20bbdcd285fa359
azhagu379 <a.palanisamy@memob.com>
2022-12-29 19:40:56 +0530

    Day-98

--- src/test/java/com/example/demoapp/UsersRepositoryTest.java
+++ src/test/java/com/example/demoapp/UsersRepositoryTest.java
@@ -19,11 +19,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class UsersRepositoryTest {
     @Autowired
     private UsersRepository userRepository;
+
     @Test
     @Order(1)
     @Rollback(value = false)
-    public  void saveUser(){
-        Users users= Users.builder()
+    public void saveUser() {
+        Users users = Users.builder()
                 .firstName("test")
                 .lastName("test")
                 .email("test@test.com")
@@ -31,47 +32,49 @@ class UsersRepositoryTest {
 
         userRepository.save(users);
         assertThat(users.getId()).isGreaterThan(0);
-        }
+    }
 
-        @Test
-        @Order(2)
-        public void getUserTest(){
-        Users users=userRepository.findById(1l).get();
+    @Test
+    @Order(2)
+    public void getUserTest() {
+        Users users = userRepository.findById(1l).get();
 
         assertThat(users.getId()).isEqualTo(1l);
 
-        }
-        @Test
-        @Order(3)
-        public void getListOfUsers(){
-        List<Users> users=userRepository.findAll();
+    }
+
+    @Test
+    @Order(3)
+    public void getListOfUsers() {
+        List<Users> users = userRepository.findAll();
         assertThat(users.size()).isGreaterThan(0);
-        }
+    }
+
     @Test
     @Order(4)
     @Rollback(value = false)
-    public void UpdateListOfUsers(){
-        Users users=userRepository.findById(1l).get();
+    public void UpdateListOfUsers() {
+        Users users = userRepository.findById(1l).get();
         users.setEmail("alagu@admin.com");
-        Users users1=userRepository.save(users);
+        Users users1 = userRepository.save(users);
         assertThat(users.getEmail()).isEqualTo("alagu@admin.com");
     }
+
     @Test
     @Order(5)
     @Rollback(value = false)
-    public void deleteUser(){
-        Users users=userRepository.findById(1l).get();
+    public void deleteUser() {
+        Users users = userRepository.findById(1l).get();
         userRepository.delete(users);
-//        userRepository.deleteById(1l);
-        Users users1=null;
-        Optional<Users> OptionalUser=userRepository.findByEmail("alagu@gmail.com");
-        if(OptionalUser.isPresent()){
-            users1=OptionalUser.get();
+
+        Users users1 = null;
+        Optional<Users> OptionalUser = userRepository.findByEmail("alagu@gmail.com");
+        if (OptionalUser.isPresent()) {
+            users1 = OptionalUser.get();
 
         }
         assertThat(users1).isNull();
 
-
     }
 
-    }
+}
